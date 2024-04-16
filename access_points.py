c2407b7ccdb8d02a2ec617a3c57d4b20c3972ab8
Sourabh Zanwar <s.zanwar@reply.de>
2024-04-16 15:26:46 +0200

    added access info

---
+++ access_points.py
@@ -0,0 +1,3 @@
+# Keys
+AZURE_OPENAI_ENDPOINT='https://frank-openai.openai.azure.com/'
+AZURE_OPENAI_API_KEY='5628ce2b594549ea84b0946f1ec31278'
--- setup_openai.py
+++ setup_openai.py
@@ -1,10 +1,9 @@
 import os
-from dotenv import dotenv_values
-
+from access_points import AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_API_KEY
 def set_env():
     #os.environ["OPENAI_API_KEY"] = dotenv_values(".env")['LLM_KEY']
-    os.environ["AZURE_OPENAI_ENDPOINT"] = dotenv_values(".env")['AZURE_OPENAI_ENDPOINT']
-    os.environ["AZURE_OPENAI_KEY"] = dotenv_values(".env")['AZURE_OPENAI_API_KEY']
+    os.environ["AZURE_OPENAI_ENDPOINT"] = AZURE_OPENAI_ENDPOINT
+    os.environ["AZURE_OPENAI_KEY"] = AZURE_OPENAI_API_KEY
 
 def reset_env():    
     #os.environ.pop('OPENAI_API_KEY')
